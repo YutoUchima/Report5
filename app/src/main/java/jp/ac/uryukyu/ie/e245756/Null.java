2afea83e8a4a19a74bd9052bcd4cb68bc6897000
Yuto Uchima <e245756@ie.u-ryukyu.ac.jp>
2024-12-11 19:22:19 +0900

    report5 step3

--- app/src/main/java/jp/ac/uryukyu/ie/e245756/Null.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e245756/Null.java
@@ -2,12 +2,8 @@ package jp.ac.uryukyu.ie.e245756;
 
 public class Null {
     public static void main(String[] args) {
-        try{
-            String str = null;
-            System.out.println(str.length());
-        }catch(NullPointerException e){
-            System.out.println("NullPointerExceptionのエラーが発生しました。");
-            System.out.println(e.getMessage());
-        }
+       String str = "壱百満";
+       int value = Integer.parseInt(str);
+       System.out.println(value);
     }
 }
