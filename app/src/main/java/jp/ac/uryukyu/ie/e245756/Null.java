e1d3320108682ec39948b731c896466fef60abe4
Yuto Uchima <e245756@ie.u-ryukyu.ac.jp>
2024-12-11 18:17:32 +0900

    report5 step1

---
+++ app/src/main/java/jp/ac/uryukyu/ie/e245756/Null.java
@@ -0,0 +1,8 @@
+package jp.ac.uryukyu.ie.e245756;
+
+public class Null {
+    public static void main(String[] args) {
+        String str = null;
+        System.out.println(str.length());
+    }
+}
